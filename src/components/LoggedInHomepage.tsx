50566f575165124dea11e8604ca0a36dbe6b5638
Francisco Simões <fd.simoes@campus.fct.unl.pt>
2021-11-24 10:29:10 +0000

    google maps

--- src/components/LoggedInHomepage.tsx
+++ src/components/LoggedInHomepage.tsx
@@ -4,13 +4,91 @@ import { Button } from "@mui/material";
 import { Link } from "react-router-dom";
 import "../styles/LoggedInHomepage.css";
 import LoggedInTopBar from "./LoggedInTopBar";
+import { compose, withProps } from "recompose";
+import {
+	withScriptjs,
+	withGoogleMap,
+	GoogleMap,
+	Marker,
+} from "react-google-maps";
+
+const MyMapComponent = compose(
+	withProps({
+		googleMapURL:
+			"https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places",
+		loadingElement: <div style={{ height: `100%` }} />,
+		containerElement: <div style={{ height: `400px` }} />,
+		mapElement: <div style={{ height: `100%` }} />,
+	}),
+	withScriptjs,
+	withGoogleMap
+)((props) => (
+	<GoogleMap defaultZoom={8} defaultCenter={{ lat: -34.397, lng: 150.644 }}>
+		(
+		<Marker position={{ lat: -34.397, lng: 150.644 }} />)
+	</GoogleMap>
+));
 
 function LoggedInHomepage() {
 	return (
 		<div>
 			<LoggedInTopBar />
+			<MyMapComponent />
 		</div>
 	);
 }
 
 export default LoggedInHomepage;
+
+/*import React from "react";
+import { compose, withProps } from "recompose";
+import {
+	withScriptjs,
+	withGoogleMap,
+	GoogleMap,
+	Marker,
+} from "react-google-maps";
+
+const MyMapComponent = compose(
+	withProps({
+		googleMapURL:
+			"https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places",
+		loadingElement: <div style={{ height: `100%` }} />,
+		containerElement: <div style={{ height: `400px` }} />,
+		mapElement: <div style={{ height: `100%` }} />,
+	}),
+	withScriptjs,
+	withGoogleMap
+)((props) => (
+	<GoogleMap defaultZoom={8} defaultCenter={{ lat: -34.397, lng: 150.644 }}>
+		(
+		<Marker position={{ lat: -34.397, lng: 150.644 }} />)
+	</GoogleMap>
+));
+
+class MyFancyComponent extends React.PureComponent {
+	state = {
+		isMarkerShown: false,
+	};
+
+	componentDidMount() {
+		this.delayedShowMarker();
+	}
+
+	delayedShowMarker = () => {
+		setTimeout(() => {
+			this.setState({ isMarkerShown: true });
+		}, 3000);
+	};
+
+	handleMarkerClick = () => {
+		this.setState({ isMarkerShown: false });
+		this.delayedShowMarker();
+	};
+
+	render() {
+		return <MyMapComponent />;
+	}
+}
+
+export default MyFancyComponent;*/
